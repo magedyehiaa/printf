2702caff3090d8def29001d946c7c11359db73e2
Maged <myeof.ii@gmail.com>
2023-10-18 01:26:08 +0300

    adding

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the chrarcter Me G oOo
+ * @c: The character.
+ *
+ * Return: success 1.
+ *
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
